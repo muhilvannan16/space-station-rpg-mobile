52ba645a314337c21eb17c2136ecc2d7826d9d7b
muhilvannan16 <muhilvannan.elavazhagan@gmail.com>
2026-07-22 11:00:01 -0400

    Fix combat rapid-tap crash and negative HP display

--- screens/combat_screen.py
+++ screens/combat_screen.py
@@ -18,33 +18,39 @@ class CombatScreen(Screen):
         super().__init__(**kwargs)
         self.enemy = None
         self.enemy_pos = None
+        self._combat_over = False
 
     def setup(self, enemy, pos, game_state=None, return_screen='game'):
         self.enemy = enemy
         self.enemy_pos = pos
         self.enemy_killed = False
+        self._combat_over = False
         self._gs = game_state or self.manager.app.game_state
         self._return_screen = return_screen
 
     def on_enter(self, *args):
         gs = self._gs
         self.enemy_name = self.enemy.name
-        self.enemy_hp = self.enemy.health
-        self.player_hp = gs.health
+        self.enemy_hp = max(0, self.enemy.health)
+        self.player_hp = max(0, gs.health)
         self.weapon_damage = get_weapon_damage(gs.inventory)
         self.defense = get_armor_defense(gs.inventory)
         self.message = f'A {self.enemy.name} attacks you!'
 
     def attack(self):
+        if self._combat_over:
+            return
+
         gs = self._gs
 
         # Player attacks
         play_sound('attack')
         self.enemy.health -= self.weapon_damage
-        self.enemy_hp = self.enemy.health
+        self.enemy_hp = max(0, self.enemy.health)
         self.message = f'You deal {self.weapon_damage} damage!'
 
         if self.enemy.health <= 0:
+            self._combat_over = True
             self.message = f'You defeated the {self.enemy.name}!'
             del gs.active_enemies[self.enemy_pos]
             gs.killed_enemies.append(list(self.enemy_pos))
@@ -56,23 +62,28 @@ class CombatScreen(Screen):
         # Enemy attacks back
         actual = calc_damage(self.enemy.damage, self.defense)
         gs.health -= actual
-        self.player_hp = gs.health
+        self.player_hp = max(0, gs.health)
         play_sound('hit')
         self.message += f'  {self.enemy.name} hits for {actual}!'
 
         if gs.health <= 0:
+            self._combat_over = True
             gs.message = 'You died!'
             Clock.schedule_once(self._game_over, 1.0)
 
     def run_away(self):
+        if self._combat_over:
+            return
+
         gs = self._gs
         actual = calc_damage(self.enemy.damage, self.defense)
         gs.health -= actual
-        self.player_hp = gs.health
+        self.player_hp = max(0, gs.health)
         play_sound('hit')
         gs.message = f'Fled from the {self.enemy.name}!'
 
         if gs.health <= 0:
+            self._combat_over = True
             Clock.schedule_once(self._game_over, 0.5)
         else:
             self._return_to_game()
